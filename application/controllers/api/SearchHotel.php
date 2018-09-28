81b04490628568715809cf86503df55008eae81e
AntoniusDoni <donioantonius@gmail.com>
2018-09-28 14:55:06 +0700

    update serach
    search hotel

--- application/controllers/api/SearchHotel.php
+++ application/controllers/api/SearchHotel.php
@@ -40,7 +40,8 @@ pi()/180))))*180/pi())*60*1.1515 ) as distance
 FROM hotel h
 LEFT JOIN voucer_travel v ON h.id_hotel=v.id_hotel
 LEFT JOIN hotel_pic p ON h.id_hotel=p.id_hotel
-WHERE 1 and v.start_date >='" . $start_date . "' or v.end_date>='" . $end_date . "'
+left JOIN detail_book on detail_book.id_voucer=v.id_voucer
+WHERE 1 and v.start_date >='" . $start_date . "' and v.end_date='" . $end_date . "' and  detail_book.id_voucer is NULL
 GROUP BY h.id_hotel 
 HAVING distance < 10 and stok>0
 limit 0,10) as X 
@@ -52,7 +53,7 @@ ORDER BY id_hotel DESC");
               INNER JOIN sub_district subD on subD.id_sub_district=h.id_sub_district
               INNER JOIN district distrct on distrct.id_district=subD.id_district
               INNER JOIN province province on province.id_province=distrct.id_province
-              WHERE province_name LIKE '" . $city . "' and v.start_date >='" . $start_date . "' or v.end_date>='" . $end_date . "'
+              WHERE province_name LIKE '" . $city . "' and v.start_date >='" . $start_date . "' and v.end_dat='" . $end_date . "' and  detail_book.id_voucer is NULL
               GROUP BY h.id_hotel 
               HAVING stok >0 limit 0,50");
         }
@@ -71,9 +72,10 @@ ORDER BY id_hotel DESC");
         $end_date = $this->get('end_date', TRUE);
         $datahotel = $this->ModelVoucher->getListVoucher("INNER JOIN hotel on hotel.id_hotel=voucer_travel.id_hotel
 INNER JOIN room_type on room_type.id_room_type=voucer_travel.id_room_type
-WHERE voucer_travel.id_hotel='1' and start_date >='".$start_date."' or end_date>'".$end_date."'
-group by room_type.id_room_type order by room_type.id_room_type DESC","id_voucer,no_voucer,hotel.id_hotel,room_type.id_room_type,
-hotel_name,name_type,start_date,end_date,expired_date,price,primary_pic");
+left JOIN detail_book on detail_book.id_voucer=voucer_travel.id_voucer
+WHERE voucer_travel.id_hotel='".$id_hotel."' and start_date >='".$start_date."' and end_date='".$end_date."'and  detail_book.id_voucer is NULL
+group by room_type.id_room_type order by room_type.id_room_type DESC","voucer_travel.id_voucer,no_voucer,hotel.id_hotel,room_type.id_room_type,
+hotel_name,name_type,start_date,end_date,expired_date,voucer_travel.price,primary_pic");
         $data = $datahotel->result();
         if(empty($data)){
           $data['info']='Data not Found';  
